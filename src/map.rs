164498ed9a88499742aedd7f4fcf36aa503aa086
Elton Dornelas <eltondornelas@gmail.com>
2022-05-19 06:21:57 -0300

    adding bracket_lib crate, adding map module, creating prelude in main

---
+++ src/map.rs
@@ -0,0 +1,7 @@
+// map-related code
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+// super:: accesses the module immediately above your module in the tree.
+// crate:: accesses the root of the tree, main.rs .
